50bff4c74bf326a20b4360bd63fe3ac70fd63edb
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-12 15:43:45 -0500

    delete django project

--- sample/sample_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from sample_app import views
-
-urlpatterns = [
-    path('', views.sample_app, name='sample_app'),
-]
--- sample/sample_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def sample_app(request):
-    return render(request, 'sample_app.html', {})

24a3f9b0af4ff75ba0b91a0842be0793c24821b9
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-12 16:48:14 -0500

    update to include / as it was failing

--- persona/urls.py
+++ persona/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from persona import views
 
 urlpatterns = [
-    path('persona', views.persona, name='persona'),
+    path('persona/', views.persona, name='persona'),
 ]

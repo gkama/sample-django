d356316fa970f5c7d7adb67a7811bcca4ee22679
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-13 08:49:59 -0500

    update to /projects

--- gtknlp/project/views.py
+++ gtknlp/project/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from project.models import Project
 
-# Create your views here.
+def project_index(request):
+    p1 = Project(
+        title='My First Project',
+        description='A web development project.',
+        technology='Django',
+        image='img/project1.png'
+        )
+    p2 = Project(
+        title='My Second Project',
+        description='A web development project.',
+        technology='Django',
+        image='img/project1.png'
+    )
+
+    projects = [p1, p2]
+
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)

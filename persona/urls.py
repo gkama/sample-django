0ddc0b298164e26d59a2133aaf67ed611373ccaa
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-13 08:11:18 -0500

    delete django shell project

--- persona/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from persona import views
-
-urlpatterns = [
-    path('persona/', views.persona, name='persona'),
-]
--- persona/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def persona(request):
-    return render(request, 'persona.html', {})
